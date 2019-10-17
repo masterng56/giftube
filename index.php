4a5f8f13b22ee2f61b56e4149eed8cc8f7da11e6
masterng56 <masterng@yandex.ru>
2019-10-17 22:48:01 +0500

    Вывел простой массив

--- index.php
+++ index.php
@@ -146,28 +146,17 @@ $user_avatar = 'img/user.jpg';
             'url'   => 'img/lot-6.jpg'
         ]
     ]; ?>
-    
+
     <footer class="main-footer">
         <nav class="nav">
             <ul class="nav__list container">
-                <li class="nav__item">
-                    <a href="all-lots.html">Доски и лыжи</a>
-                </li>
-                <li class="nav__item">
-                    <a href="all-lots.html">Крепления</a>
-                </li>
-                <li class="nav__item">
-                    <a href="all-lots.html">Ботинки</a>
-                </li>
-                <li class="nav__item">
-                    <a href="all-lots.html">Одежда</a>
-                </li>
-                <li class="nav__item">
-                    <a href="all-lots.html">Инструменты</a>
-                </li>
-                <li class="nav__item">
-                    <a href="all-lots.html">Разное</a>
-                </li>
+                <?php foreach ($categories as $value) { ?>
+                    <li class="nav__item">
+                        <a href="all-lots.html"><?php echo $value; ?></a>
+                    </li>
+                <?php } ?>
+
+
             </ul>
         </nav>
         <div class="main-footer__bottom container">
