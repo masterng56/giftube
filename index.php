aeb77b1c06b844e24d72568a81cfdce80ed13045
masterng56 <masterng@yandex.ru>
2019-10-17 22:07:59 +0500

    Закончил масивы

--- index.php
+++ index.php
@@ -114,8 +114,39 @@ $user_avatar = 'img/user.jpg';
             'cat'   => 'Доски и лыжи',
             'price' => '10999',
             'url'   => 'img/lot-1.jpg'
+        ],
+        1 => [
+            'title' => 'DC Ply Mens 2016/2017 Snowboard',
+            'cat'   => 'Доски и лыжи',
+            'price' => '159999',
+            'url'   => 'img/lot-2.jpg'
+        ],
+        2 => [
+            'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+            'cat'   => 'Крепления',
+            'price' => '8000',
+            'url'   => 'img/lot-3.jpg'
+        ],
+        3 => [
+            'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
+            'cat'   => 'Ботинки',
+            'price' => '10999',
+            'url'   => 'img/lot-4.jpg'
+        ],
+        4 => [
+            'title' => 'Куртка для сноуборда DC Mutiny Charocal',
+            'cat'   => 'Одежда',
+            'price' => '7500',
+            'url'   => 'img/lot-5.jpg'
+        ],
+        5 => [
+            'title' => 'Маска Oakley Canopy',
+            'cat'   => 'Разное',
+            'price' => '5400',
+            'url'   => 'img/lot-6.jpg'
         ]
     ]; ?>
+    
     <footer class="main-footer">
         <nav class="nav">
             <ul class="nav__list container">
